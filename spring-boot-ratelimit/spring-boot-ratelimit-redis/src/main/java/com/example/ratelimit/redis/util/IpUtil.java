2d289094c3c1e4b98370e2aa62b2e9d3f3066c5d
wzy <2630559606@qq.com>
2023-09-12 21:26:19 +0800

    init

--- spring-boot-ratelimit/spring-boot-ratelimit-redis/src/main/java/com/example/ratelimit/redis/util/IpUtil.java
+++ spring-boot-ratelimit/spring-boot-ratelimit-redis/src/main/java/com/example/ratelimit/redis/util/IpUtil.java
@@ -1,11 +1,10 @@
 package com.example.ratelimit.redis.util;
 
 import cn.hutool.core.util.StrUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
